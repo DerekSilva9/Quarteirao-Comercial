7b2da431549b79d45121673e81f89ec8426c45da
DerekSilva <88068277+DerekSilva9@users.noreply.github.com>
2024-08-01 23:47:15 -0300

    Ajustes

--- welcome/login.js
+++ welcome/login.js
@@ -1,19 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('loginForm');
+function validateLogin(event) {
+    event.preventDefault(); // Evita o envio normal do formulário
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Impede o envio do formulário
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-        // Obter os valores dos campos de email e senha
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
+    const validEmail = 'adtecinfor@gmail.com';
+    const validPassword = '12345678';
 
-        // Verificação básica de email e senha
-        if (email === 'user@example.com' && password === 'password123') {
-            alert('Login bem-sucedido!');
-            window.location.href = '../ecommerce/home.html';
-        } else {
-            alert('Email ou senha inválidos!');
-        }
-    });
-});
+    if (email === validEmail && password === validPassword) {
+        window.location.href = '/ecommerce/home.html';
+    } else {
+        alert('Email ou senha incorretos');
+    }
+}
